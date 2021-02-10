3c5ad5eb65f7601fb71850cc62acee9496c24490
vpMac1.14Catalina <vpmac1.14catalina@Mac-de-vpMac114Catalina.local>
2021-02-10 18:25:25 +0100

    waiting screen

--- Project_IOS/Project_IOS/Scene/HomeViewController.swift
+++ Project_IOS/Project_IOS/Scene/HomeViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.imageView.image = UIImage(named:"basket1.jpg")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.imageView.image = UIImage(named:"football.jpg")
+        }
         DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+            print()
             let chooseSportViewController = ChooseSportViewController(nibName: "ChooseSportViewController", bundle: nil)
             self.navigationController?.pushViewController(chooseSportViewController, animated: true)
         }
